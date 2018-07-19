f055d0ea95835e70090c5d013be34700a419f690
HemaGRepo <hema.g@capgemini.com>
2018-07-19 14:35:40 +0530

    Commit4

---
+++ Employee.java
@@ -0,0 +1,12 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Counting..."+i);
+		}
+		System.out.println("Welcome to Employee Management System");
+	}
+
+}
---
+++ TaxInfo.java
@@ -0,0 +1,10 @@
+
+public class TaxInfo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=5;i++)
+		System.out.println("Tax Paid"+i);
+
+	}
+
+}
